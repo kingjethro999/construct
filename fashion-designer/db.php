d60046815df4c809dcfebba5a439f0484ecfad69
firstlady-glitch <jerryfirstlady@gmail.com>
2026-06-21 23:13:18 +0100

    me

--- fashion-designer/db.php
+++ fashion-designer/db.php
@@ -1,10 +1,10 @@
 <?php
-$host = getenv('DB_HOST') ?: "localhost";
-$user = getenv('DB_USER') ?: "root";
-$pass = getenv('DB_PASS') !== false ? getenv('DB_PASS') : "";
-$name = getenv('DB_NAME') ?: "fashion_designer";
-$port = getenv('DB_PORT') ?: 3306;
-$ssl  = getenv('DB_SSL') === 'true';
+$host = "mysql-95ad972-thisiskingjethro-3be3.b.aivencloud.com";
+$user = "avnadmin";
+$pass = "AVNS__QSThIrRML7Pl85aWhR";
+$name = "defaultdb";
+$port = 21723;
+$ssl  = true;
 
 $conn = mysqli_init();
 $flags = $ssl ? MYSQLI_CLIENT_SSL : 0;
